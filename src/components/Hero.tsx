d95083d1fe8e2fdb883d00b5890bc6c6893b61d0
Sakarias Näslund <sakarias.naslund@gmail.com>
2025-01-14 20:20:07 +0100

    Added hero section and changed styling

---
+++ src/components/Hero.tsx
@@ -0,0 +1,15 @@
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <h1 className="hero-h1">En lekstuga för React.js</h1>
+        <p className="hero-p">
+          Det här är en hemsida med ett enda syfte - att lära sig skapa
+          webbapplikationer med React
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
